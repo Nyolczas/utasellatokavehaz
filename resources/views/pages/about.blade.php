cc8296cbd113339cedd70be6f5ab8c89f7a9116c
Nyolczas <nyolczas@gmail.com>
2020-03-09 21:36:47 +0100

    about update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,17 +1,23 @@
-<div class="container py-4">
+<div class="container py-4 mb-4">
     <div class="row flex-wrap">
         <div class="col-12 col-md-6 col-lg-7 col-xl-8">
-            <div class="keret">
+            <div class="keret mb-0">
                 <div class="keret-bel p-4">
                     <h2 class="text-center heading mb-4">Köszöntjük az Utasellátó Kávéházban</h2>
                     <p>
                         A Magyar Vasúttörténeti Park <b>Magyarország egyik legizgalmasabb interaktív parkja.</b>
-A több, mint százéves mozdonycsodák között sétálva az ember úgy érezheti, visszarepül az időben. Ennek fényében épült fel a park területén az <b>Utasellátó Kávéház</b>  is, ahol az élmények sokasága után megpihenhet az ember.
+A több, mint százéves mozdony-csodák között sétálva az ember úgy érezheti, visszarepül az időben. Ennek fényében épült fel a park területén az <b>Utasellátó Kávéház</b>  is, ahol az élmények sokasága után megpihenhet az ember.
                     </p>
                     <p>
-                        Akár egy finom frissensültre vágyik, akár egy különleges kávéra kézműves süteménnyel, vagy egy igazi, hamisíthatatlan csavaros fagyira, mi mindig szeretettel és mosollyal fogadjuk vendégeinket. Számunkra a legfontosabb, hogy a család összes tagja, a legapróbbaktól a legidősebbekig megelégedettséggel távozzon, és a következő kirándulásuk alkalmával újra betérjenek hozzánk is.
+                        Akár egy finom frissensültre vágyik, akár egy különleges kávéra, kézműves süteményre, vagy egy igazi, hamisíthatatlan csavaros fagyira, mi mindig szeretettel és mosollyal fogadjuk vendégeinket. Számunkra a legfontosabb, hogy a család összes tagja, a legapróbbaktól a legidősebbekig megelégedettséggel távozzon, és a következő kirándulásuk alkalmával újra betérjenek hozzánk is.
                     </p>
                 </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-5 col-xl-4">
+            <div class="blackboard p-4 text-center">
+                <h2 class="heading text-gold">Nyitvatartás</h2>
+                <h3><b>Április 1 - Október 31.</b></h3>
             </div>
         </div>
     </div>
